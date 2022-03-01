91b019c713029ffdff81bcb83fff0efbf90bf104
alexh <alex.hammonnd003@gmail.com>
2022-03-01 01:26:23 +0000

    Un-depreciated logger

--- src/main/java/net/bruhitsalex/tauq/misc/Logger.java
+++ src/main/java/net/bruhitsalex/tauq/misc/Logger.java
@@ -1,29 +1,27 @@
 package net.bruhitsalex.tauq.misc;
 
-import org.apache.log4j.Priority;
-
 public class Logger {
 
-    private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getRootLogger();
+    private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(Logger.class);
 
     public static void log(LoggerType type, String text) {
-        LOGGER.log(Priority.INFO, "[" + type.name().toUpperCase() + "] " + text);
+        LOGGER.info("[" + type.name().toUpperCase() + "] " + text);
     }
 
     public static void warn(LoggerType type, String text) {
-        LOGGER.log(Priority.WARN, "[" + type.name().toUpperCase() + "] " + text);
+        LOGGER.warn("[" + type.name().toUpperCase() + "] " + text);
     }
 
     public static void error(LoggerType type, String text) {
-        LOGGER.log(Priority.ERROR, "[" + type.name().toUpperCase() + "] " + text);
+        LOGGER.error("[" + type.name().toUpperCase() + "] " + text);
     }
 
     public static void debug(LoggerType type, String text) {
-        LOGGER.log(Priority.DEBUG, "[" + type.name().toUpperCase() + "] " + text);
+        LOGGER.debug("[" + type.name().toUpperCase() + "] " + text);
     }
 
     public static void fatal(LoggerType type, String text) {
-        LOGGER.log(Priority.FATAL, "[" + type.name().toUpperCase() + "] " + text);
+        LOGGER.fatal("[" + type.name().toUpperCase() + "] " + text);
     }
 
 }
