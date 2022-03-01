51a955a19a28c18face168407f1eb099683f9d26
Alex Hammond <jaffaapp@gmail.com>
2022-03-01 01:11:36 +0000

    Main run method

--- src/main/java/net/bruhitsalex/tauq/Main.java
+++ src/main/java/net/bruhitsalex/tauq/Main.java
@@ -1,12 +1,14 @@
 package net.bruhitsalex.tauq;
 
+import net.bruhitsalex.tauq.graphics.GLFWHandler;
+
 public class Main {
 
     /***
      * Testing main method
      */
     public static void main(String[] args) {
-
+        GLFWHandler.run();
     }
 
 }
