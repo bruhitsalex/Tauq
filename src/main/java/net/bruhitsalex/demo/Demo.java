aebc6c5aaa1fb4d90298b4c01dd830599eef955e
alexh <alex.hammonnd003@gmail.com>
2022-03-02 03:58:18 +0000

    Changed demo

--- src/main/java/net/bruhitsalex/demo/Demo.java
+++ src/main/java/net/bruhitsalex/demo/Demo.java
@@ -1,22 +1,22 @@
 package net.bruhitsalex.demo;
 
 import net.bruhitsalex.tauq.graphics.TauqWindow;
-import net.bruhitsalex.tauq.graphics.events.Listeners;
-import net.bruhitsalex.tauq.graphics.events.events.AfterInitialisedEvent;
+import net.bruhitsalex.tauq.graphics.events.io.MouseClickEvent;
+import net.bruhitsalex.tauq.misc.Log;
 
-public class Demo implements AfterInitialisedEvent {
+public class Demo implements MouseClickEvent {
 
     private TauqWindow window;
 
     public void start() {
-        Listeners.AFTER_INITIALISED_LISTENERS.add(this);
         this.window = new TauqWindow();
-        window.run();
+        window.start();
+        window.getEventHandler().getListeners().MOUSE_CLICK_LISTENERS.add(this);
     }
 
     @Override
-    public void afterInitialized() {
-
+    public void onMouseClick(int button) {
+        Log.debug("FPS: " + window.getRenderer().getFPS());
     }
 
 }
