74d40fa7b533b8909bc37bb4c6f33de8bda2c14f
alexh <alex.hammonnd003@gmail.com>
2022-03-02 03:59:13 +0000

    Windows run in a renderthread, and now support multiple windows

--- src/main/java/net/bruhitsalex/tauq/graphics/TauqWindow.java
+++ src/main/java/net/bruhitsalex/tauq/graphics/TauqWindow.java
@@ -1,14 +1,17 @@
 package net.bruhitsalex.tauq.graphics;
 
-import net.bruhitsalex.tauq.graphics.events.EventHandlers;
+import net.bruhitsalex.tauq.graphics.events.EventHandler;
+import net.bruhitsalex.tauq.graphics.events.window.PixelCoords;
 import net.bruhitsalex.tauq.misc.Log;
 import net.bruhitsalex.tauq.misc.LoggerType;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.system.MemoryStack;
 
+import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
 
 import static org.lwjgl.glfw.GLFW.*;
@@ -17,19 +20,36 @@ import static org.lwjgl.system.MemoryUtil.*;
 
 public class TauqWindow {
 
+    private static boolean glfwInitialised = false;
+    private static volatile boolean initialisingGlfw = false;
+
     private static final int INIT_DISPLAY_WIDTH = 1280;
     private static final int INIT_DISPLAY_HEIGHT = 720;
     private static final String INIT_DISPLAY_TITLE = "Tauq";
 
     private long window;
-    private OpenGLHandler openGLHandler;
+    private OpenGLHandler renderer;
+    private EventHandler eventHandlers;
 
-    public void run() {
+    private Thread renderThread;
+
+    private int displayWidth;
+    private int displayHeight;
+
+    public void start() {
+        this.renderThread = new Thread(this::run);
+        this.eventHandlers = new EventHandler();
+        this.displayWidth = INIT_DISPLAY_WIDTH;
+        this.displayHeight = INIT_DISPLAY_HEIGHT;
+        renderThread.start();
+    }
+
+    private void run() {
         Log.log(LoggerType.GLFW, "Running LWJGL version " + Version.getVersion());
         if (init()) {
-            EventHandlers.afterInitialised();
-            this.openGLHandler = new OpenGLHandler();
-            openGLHandler.loop(this);
+            this.eventHandlers.afterInitialised();
+            this.renderer = new OpenGLHandler();
+            this.renderer.loop(getInstance());
         }
     }
 
@@ -37,14 +57,25 @@ public class TauqWindow {
         Log.log(LoggerType.GLFW, "Initialising window...");
         GLFWErrorCallback.createPrint(System.err).set();
 
-        if (!GLFW.glfwInit()) {
-            Log.error(LoggerType.GLFW, "Unable to initialise GLFW...");
-            return false;
+        while (initialisingGlfw) {
+            Thread.onSpinWait();
+        }
+
+        if (!glfwInitialised && !initialisingGlfw) {
+            initialisingGlfw = true;
+            if (!GLFW.glfwInit()) {
+                Log.error(LoggerType.GLFW, "Unable to initialise GLFW...");
+                return false;
+            }
+            glfwInitialised = true;
+            initialisingGlfw = false;
         }
 
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
         window = glfwCreateWindow(INIT_DISPLAY_WIDTH, INIT_DISPLAY_HEIGHT, INIT_DISPLAY_TITLE, NULL, NULL);
 
@@ -85,14 +116,67 @@ public class TauqWindow {
 
     private void setupCallbacks() {
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if (action == GLFW_PRESS) {
-                EventHandlers.onKeyPress(key);
+            switch (action) {
+                case GLFW_PRESS:
+                    eventHandlers.onKeyPress(key);
+                    return;
+                case GLFW_RELEASE:
+                    eventHandlers.onKeyRelease(key);
             }
         });
+
+        glfwSetScrollCallback(window, (l, v, dir) -> {
+            eventHandlers.onMouseScroll(dir == 1.0);
+        });
+
+        glfwSetMouseButtonCallback(window, (l, mouseButton, action, i2) -> {
+            switch (action) {
+                case GLFW_PRESS:
+                    eventHandlers.onMousePress(mouseButton);
+                    return;
+                case GLFW_RELEASE:
+                    eventHandlers.onMouseRelease(mouseButton);
+            }
+        });
+
+        glfwSetWindowSizeCallback(window, (l, width, height) -> {
+            this.displayWidth = width;
+            this.displayHeight = height;
+        });
+    }
+
+    private TauqWindow getInstance() {
+        return this;
     }
 
     public long getWindow() {
         return window;
+    }
+
+    public OpenGLHandler getRenderer() {
+        return renderer;
+    }
+
+    public PixelCoords getMouseXY() {
+        DoubleBuffer coords = BufferUtils.createDoubleBuffer(2);
+        glfwGetCursorPos(window, coords, coords);
+        return new PixelCoords(coords.get(0), coords.get(1));
+    }
+
+    public EventHandler getEventHandler() {
+        return eventHandlers;
+    }
+
+    public Thread getRenderThread() {
+        return renderThread;
+    }
+
+    public int getDisplayWidth() {
+        return displayWidth;
+    }
+
+    public int getDisplayHeight() {
+        return displayHeight;
     }
 
 }
