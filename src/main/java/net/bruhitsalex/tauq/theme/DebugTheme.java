0fb38c8cb5aaff612d4d26b3f41dd00bc799b00b
alexh <alex.hammonnd003@gmail.com>
2022-03-03 04:23:22 +0000

    Themes added

---
+++ src/main/java/net/bruhitsalex/tauq/theme/DebugTheme.java
@@ -0,0 +1,17 @@
+package net.bruhitsalex.tauq.theme;
+
+import java.awt.*;
+
+public class DebugTheme implements Theme {
+
+    @Override
+    public Color getTPanelBackgroundColor() {
+        return Color.cyan;
+    }
+
+    @Override
+    public Color getTPanelBorderColor() {
+        return Color.blue;
+    }
+
+}
---
+++ src/main/java/net/bruhitsalex/tauq/theme/Theme.java
@@ -0,0 +1,10 @@
+package net.bruhitsalex.tauq.theme;
+
+import java.awt.*;
+
+public interface Theme {
+
+    Color getTPanelBackgroundColor();
+    Color getTPanelBorderColor();
+
+}
