ebc427bad4da6b1dbcc474c4b96262230a3c32a9
alexh <alex.hammonnd003@gmail.com>
2022-03-03 04:24:22 +0000

    Added drawing border

--- src/main/java/net/bruhitsalex/tauq/graphics/utils/Rectangle.java
+++ src/main/java/net/bruhitsalex/tauq/graphics/utils/Rectangle.java
@@ -6,9 +6,14 @@ import java.awt.*;
 
 import static org.lwjgl.opengl.GL11.*;
 
+@SuppressWarnings("Duplicates")
 public class Rectangle {
 
     public static void drawRect(int left, int top, int right, int bottom, Color color) {
+        if (color.getAlpha() != 255) {
+            glEnable(GL11.GL_BLEND);
+        }
+
         ColorUtils.setColor(color);
         glBegin(GL_QUADS);
         glVertex2f(left, top);
@@ -16,12 +21,29 @@ public class Rectangle {
         glVertex2f(right, bottom);
         glVertex2f(left, bottom);
         glEnd();
+
+        if (color.getAlpha() != 255) {
+            glDisable(GL11.GL_BLEND);
+        }
     }
 
-    public static void drawBlendedRect(int left, int top, int right, int bottom, Color color) {
-        glEnable(GL11.GL_BLEND);
-        drawRect(left, top, right, bottom, color);
-        glDisable(GL11.GL_BLEND);
+    public static void drawRectBorders(int left, int top, int right, int bottom, int width, Color color) {
+        if (color.getAlpha() != 255) {
+            glEnable(GL11.GL_BLEND);
+        }
+
+        ColorUtils.setColor(color);
+        glLineWidth(width);
+        glBegin(GL_LINE_LOOP);
+        glVertex2f(left, top);
+        glVertex2f(right, top);
+        glVertex2f(right, bottom);
+        glVertex2f(left, bottom);
+        glEnd();
+
+        if (color.getAlpha() != 255) {
+            glDisable(GL11.GL_BLEND);
+        }
     }
 
 }
