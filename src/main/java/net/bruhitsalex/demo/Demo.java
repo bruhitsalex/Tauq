e7cfe6ba115a9dec4ceb2f6a229e41edc5b19c96
alexh <alex.hammonnd003@gmail.com>
2022-03-03 04:23:37 +0000

    Removed test panel

--- src/main/java/net/bruhitsalex/demo/Demo.java
+++ src/main/java/net/bruhitsalex/demo/Demo.java
@@ -1,5 +1,6 @@
 package net.bruhitsalex.demo;
 
+import net.bruhitsalex.tauq.components.panels.TPanel;
 import net.bruhitsalex.tauq.graphics.TauqWindow;
 import net.bruhitsalex.tauq.graphics.events.io.MouseClickEvent;
 import net.bruhitsalex.tauq.misc.Log;
@@ -11,6 +12,7 @@ public class Demo implements MouseClickEvent {
     public void start() {
         this.window = new TauqWindow();
         window.start();
+        window.getMainPanel().add(new TPanel(10, 20, 26, 82));
         window.getEventHandler().getListeners().MOUSE_CLICK_LISTENERS.add(this);
     }
 
